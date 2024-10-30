60ce5763fb7e77281cc9494e2a93818ffa623d99
Eren Kankılınç <92270328+ReinhardVA@users.noreply.github.com>
2024-10-30 20:16:03 +0300

    Add files via upload

---
+++ Hafta 4/SpawnManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemy;
+    [SerializeField] private GameObject _enemyContainer;
+    
+    private bool _isSpawning = false;
+    IEnumerator SpawnRoutine(){
+        while(_isSpawning == false){
+            Vector3 _spanwPoint = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            GameObject newEnemy = Instantiate(_enemy, _spanwPoint, Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
+            yield return new WaitForSeconds(5.0f);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void gameOver(){
+        _isSpawning = true;
+    }
+}
